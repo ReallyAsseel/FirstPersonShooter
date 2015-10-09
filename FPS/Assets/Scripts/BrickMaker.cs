5d8fc9711fda6224fcfce5795bc79794c105481d
ReallyAsseel <TheDeveloper@Asseels-MacBook-Pro-2.local>
2015-10-09 10:21:50 -0400

    BRICK WALL ASSIGNMENT
    8x8 Brick Wall instantiating

---
+++ FPS/Assets/Scripts/BrickMaker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BrickMaker : MonoBehaviour {
+
+	GameObject[] bricks;
+	// Use this for initialization
+	void Start () {
+		bricks = new GameObject[64];
+		bricks[0] = (GameObject)GameObject.Instantiate(Resources.Load("Prefabs/Brick"), new Vector3(74f, -1.6f, 79f), Quaternion.identity);
+
+		for(int i = 1; i <= 7; i++) {
+			bricks[i] = (GameObject)(GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/Brick"), bricks[0].transform.position + new Vector3(bricks[0].GetComponent<Collider>().bounds.size.x * i + 0.01f,0f,0f), Quaternion.identity));
+			for(int a = 1; a <= 8; a++) {
+				bricks[a] = (GameObject)(GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/Brick"), bricks[0].transform.position + new Vector3(bricks[0].GetComponent<Collider>().bounds.size.x * i + 0.01f,bricks[0].GetComponent<Collider>().bounds.size.y * a + 0.01f,0f), Quaternion.identity));
+			}
+		}
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
