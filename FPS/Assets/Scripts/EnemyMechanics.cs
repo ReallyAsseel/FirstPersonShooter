3f3c46ed40d32f0a909a36aa57a894b47f5ed456
ReallyAsseel <TheDeveloper@Asseels-MacBook-Pro-2.local>
2015-11-06 13:19:24 -0500

    Enemy AI
    Midterm milestone

--- FPS/Assets/Scripts/EnemyMechanics.cs
+++ FPS/Assets/Scripts/EnemyMechanics.cs
@@ -5,8 +5,9 @@ public class EnemyMechanics : MonoBehaviour {
     public float health, speed, damage;
     public string enemyType;
     public bool readyToAttack, canMove;
-    public float timer, enemyROF, wanderTimer, timeBetweenMoves;
+    public float timer, enemyROF, wanderTimer, timeBetweenMoves, rotationDampening;
     public Vector3 randomPosition;
+	public GameObject player;
     public enum States
     {
         Chase,
@@ -16,6 +17,8 @@ public class EnemyMechanics : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+		rotationDampening = 1.5f;
+		player = GameObject.Find("Player");
         randomPosition = new Vector3((int)Random.Range(transform.position.x - 10, transform.position.x + 15), 0f, (int)Random.Range(transform.position.z - 15, transform.position.z + 10));
         enemyROF = 1f;
         wanderTimer = 0f;
@@ -36,24 +39,27 @@ public class EnemyMechanics : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		if (transform.position.y < -100) {
 			Destroy(gameObject);
 		}
+
+		if(CheckForPlayer(player))
+		{
+			currentState = States.Chase;
+		} else {
+			currentState = States.Patrol;
+		}
+
         switch(currentState)
         {
             case States.Patrol:
                 Wander();
-                if(CheckForPlayer(GameObject.Find("Player")))
-                {
-                    currentState = States.Chase;
-                } else
-                {
-                    currentState = States.Patrol;
-                }
+
                 break;
             case States.Chase:
-                TowardsPlayer(GameObject.Find("Player"));
+            	TowardsPlayer(GameObject.Find("Player"));
+				//LookAtPlayer(player);
                 Attack(readyToAttack);
                 break;
         }
@@ -63,13 +69,13 @@ public class EnemyMechanics : MonoBehaviour {
     void Wander()
     {
         Debug.DrawLine(transform.position, randomPosition);
-        if (Vector3.Distance(randomPosition, transform.position) > 4f)
+        if (Vector3.Distance(randomPosition, transform.position) > 4f) // Player distance
         {
-            speed = 8f;
+            speed = 8f; //Enemy Speed. If the enemy has not reached its random destination for patrol, it will keep going
             transform.position += new Vector3(-transform.position.x + randomPosition.x, 0f, -transform.position.z + randomPosition.z).normalized * speed * Time.deltaTime;
         } else
         {
-            speed = 0f;
+            speed = 0f; //If the enemy has reached its random patrol destination it will stop
         }
         if(wanderTimer <= Random.Range(1f, 5f))
         {
@@ -77,37 +83,42 @@ public class EnemyMechanics : MonoBehaviour {
         } else
         {
             randomPosition = new Vector3((int)Random.Range(transform.position.x - 10, transform.position.x + 15), 00f, (int)Random.Range(transform.position.z - 15, transform.position.z + 10 ));
-            wanderTimer = 0f;
+            wanderTimer = 0f; //Finally it will find another random position
         }
     }
     
     bool CheckForPlayer(GameObject player)
     {
-        if(Vector3.Distance(transform.position, player.transform.position) <= 20f)
+        if(Vector3.Distance(transform.position, player.transform.position) <= (25f + (10f/player.GetComponent<PlayerController>().health)*20)) //Check if enemy sees the player in sight
         {
-            return true;
+            return true; 
         } else
         {
             return false;
         }
     }
 
+	void LookAtPlayer(GameObject player) {
+		Quaternion rotation = Quaternion.LookRotation(player.transform.position - transform.position);
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDampening * rotationDampening);
+	}
+
     void TowardsPlayer(GameObject player)
     {
-        transform.LookAt(player.transform);
+        transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
         transform.position += transform.forward * speed * Time.deltaTime;
         if (Vector3.Distance(transform.position, player.transform.position) >= 2.5f && timer == 0)
         {
             readyToAttack = false;
             speed = 8f;
             GetComponentInChildren<Animator>().SetBool("Attack", false);
-
         }
         else
         {
             speed = 0f;
             readyToAttack = true;
-        }
+	        
+		}
     }
 
     void Attack(bool isReady)
